dfb1469582cb3351fc68746102926b29a6c25a24
Arine <peppydays@gmail.com>
2023-08-30 16:55:19 +0900

    Fixed create cluster script to exit when failed to create cluster

--- gitops/bootstrap/phase-0/create-cluster.sh
+++ gitops/bootstrap/phase-0/create-cluster.sh
@@ -1,3 +1,9 @@
 kind create cluster --name local --config local.yaml
+
+if [ $? -ne 0 ]; then
+  echo "Error creating Kubernetes cluster. Exiting."
+  exit 1
+fi
+
 kubectx kind-local
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
